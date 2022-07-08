b087ec500b9dd8916e4dd0aa69febb82f0f62fe6
Pasha127 <pasha.account@gmail.com>
2022-07-08 10:11:00 +0200

    e

--- project.js
+++ project.js
@@ -8,3 +8,13 @@ console.log(test);
 header("B")
 let sum = 10+20;
 console.log(sum);
+
+header("C");
+let random = Math.floor(Math.random()*10)+ Math.floor(Math.random()*10);
+console.log(random);
+
+header("D");
+let me = {name:`Paul`, surname:`Levitsky`, age:30};
+console.log(me);
+
+header("E");
